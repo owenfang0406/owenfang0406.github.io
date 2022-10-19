5f8d38e7e6c638b5c274e0ad3c9140cfa6ba0040
Owen Fang <ken5475ht@gmail.com>
2022-10-20 00:49:45 +0800

    Week5 4th version

--- Practice/Week5/README.md
+++ Practice/Week5/README.md
@@ -40,4 +40,77 @@ SELECT COUNT(*) id from member;
 SELECT SUM(follow_count) from member;
 
 SELECT AVG(follow_count) from member;
+---
 
+>需求5 Join 兩張 Table 查詢
+---
+![需求5](https://github.com/owenfang0406/owenfang0406.github.io/blob/main/Practice/Week5/4.png)
+---
+>建立表格 schema
+
+create table message (
+
+    id bigint primary key auto_increment,
+
+    member_id bigint not null,
+
+    content varchar(255) not null,
+
+    like_count int not null default 0,
+
+    time datetime not null default current_timestamp
+);
+
+>新增表格資料
+
+insert into message(id, member_id, content, like_count)values (1, '2', '好讚喔', 50);
+
+insert into message(id, member_id, content, like_count)values (2, '5', '問世堅情是何物', 509);
+
+insert into message(id, member_id, content, like_count)values (3, '2', '南北菜蟲一起串連', 550);
+
+insert into message(id, member_id, content, like_count)values (4, '3', '就是這麼簡單，八個字', 590);
+
+insert into message(id, member_id, content, like_count)values (5, '1', '你進土城看守所得時候，記得先吞曲棍球', 5220);
+
+insert into message(id, member_id, content, like_count)values (6, '4', '小兒科啦', 70);
+
+insert into message(id, member_id, content, like_count)values (7, '1', 'Over my dead body', 960);
+
+insert into message(id, member_id, content, like_count)values (8, '3', '愚人的問題，智者無法回答', 790);
+
+insert into message(id, member_id, content, like_count)values (9, '5', '沒錢用不等於缺錢，停工不等於缺工，沒電不等於缺電', 9990);
+
+insert into message(id, member_id, content, like_count)values (10, '2', '我沒有去汽車旅館拉', 10002);
+
+>配合 join 查詢所有留言並顯示username
+
+select member.id, member.name, member.username, message.member_id, message.content from message
+
+inner join member
+
+on message.member_id = member.id;
+---
+![需求5](https://github.com/owenfang0406/owenfang0406.github.io/blob/main/Practice/Week5/5.png)
+---
+>配合 join 顯示 username = 'test2' 由於先前需求 username test update成 test2
+
+select member.id, member.username, message.member_id, message.content from message
+
+inner join member
+
+on message.member_id = member.id
+
+where member.username = 'test2';
+
+>配合 join 查詢出 username = test2 且 按讚數平均值
+
+select member.username, message.member_id, avg(message.like_count) from message
+
+inner join member
+
+on message.member_id = member.id
+
+where username = 'test2'
+
+group by member_id;
---
+++ Practice/Week5/dump_file.sql
@@ -0,0 +1,82 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for macos12.6 (arm64)
+--
+-- Host: localhost    Database: website
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `member`
+--
+
+DROP TABLE IF EXISTS `member`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `member` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `follow_count` int NOT NULL DEFAULT '0',
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `member`
+--
+
+LOCK TABLES `member` WRITE;
+/*!40000 ALTER TABLE `member` DISABLE KEYS */;
+INSERT INTO `member` VALUES (1,'tester','test2','test',5,'2022-10-18 21:36:09'),(2,'adminr','admint','12345t',55,'2022-10-18 21:38:06'),(3,'Owen','Owen0406t','67890zt',53,'2022-10-18 21:39:18'),(4,'User_Will','willt','1qaz2wsxt',455,'2022-10-18 21:40:13'),(5,'User_Tom\"','Tom','aaabbb',4455,'2022-10-18 21:41:30');
+/*!40000 ALTER TABLE `member` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `message`
+--
+
+DROP TABLE IF EXISTS `message`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `message` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `member_id` bigint NOT NULL,
+  `content` varchar(255) NOT NULL,
+  `like_count` int NOT NULL DEFAULT '0',
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `message`
+--
+
+LOCK TABLES `message` WRITE;
+/*!40000 ALTER TABLE `message` DISABLE KEYS */;
+INSERT INTO `message` VALUES (1,2,'好讚喔',50,'2022-10-19 19:28:53'),(2,5,'問世堅情是何物',509,'2022-10-19 19:36:26'),(3,2,'南北菜蟲一起串連',550,'2022-10-19 19:36:26'),(4,3,'就是這麼簡單，八個字',590,'2022-10-19 19:36:26'),(5,1,'你進土城看守所得時候，記得先吞曲棍球',5220,'2022-10-19 19:36:26'),(6,4,'小兒科啦',70,'2022-10-19 19:36:26'),(7,1,'Over my dead body',960,'2022-10-19 19:36:26'),(8,3,'愚人的問題，智者無法回答',790,'2022-10-19 19:36:26'),(9,5,'沒錢用不等於缺錢，停工不等於缺工，沒電不等於缺電',9990,'2022-10-19 19:36:26'),(10,2,'我沒有去汽車旅館拉',10002,'2022-10-19 19:36:28');
+/*!40000 ALTER TABLE `message` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-20  0:48:56
